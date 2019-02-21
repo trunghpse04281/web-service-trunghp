aef0b0e3cd3b412adb9e3d74120e3bdce4025c00
trunghpse04281 <33221143+trunghpse04281@users.noreply.github.com>
2019-02-21 13:33:31 +0700

    update DB connection

--- api/db.js
+++ api/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST || "sql12.freemysqlhosting.net",
-    user: process.env.DB_USER || "sql12278762",
-    password: process.env.DB_PASS || "ulWGBAqUxJ",
-    database: process.env.DB_NAME || "sql12278762"
+    host: process.env.DB_HOST || "remotemysql.com",
+    user: process.env.DB_USER || "NCdebdV3oK",
+    password: process.env.DB_PASS || "42yte9ZobR",
+    database: process.env.DB_NAME || "NCdebdV3oK",
+    port: process.env.DB_PORT || 3306
 });
 
 // const db = mysql.createConnection({
@@ -19,7 +20,5 @@ db.connect(err => {
     console.log('DB Connected!');
 });
 module.exports = db;
-
-// ALTER USER 'sql12278762'@'sql12.freemysqlhosting.net' IDENTIFIED WITH mysql_native_password BY 'ulWGBAqUxJ';
 
 
