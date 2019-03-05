c1f1c46fca0608093a3c9d343dc6e9c2e3ba157b
trunghpse04281 <33221143+trunghpse04281@users.noreply.github.com>
2019-03-05 12:15:26 +0700

    hh

--- api/db.js
+++ api/db.js
@@ -1,19 +1,19 @@
 const mysql = require('mysql');
 
-// const db = mysql.createConnection({
-//     host: process.env.DB_HOST || "remotemysql.com",
-//     user: process.env.DB_USER || "XwcCLxaOuP",
-//     password: process.env.DB_PASS || "f6mbcvHCko",
-//     database: process.env.DB_NAME || "XwcCLxaOuP",
-//     port: process.env.DB_PORT || 3306
-// });
-
 const db = mysql.createConnection({
-    server: "localhost",
-    database: "android_final_project",
-    user: "root",
-    password: "123456"
-})
+    host: process.env.DB_HOST || "remotemysql.com",
+    user: process.env.DB_USER || "XwcCLxaOuP",
+    password: process.env.DB_PASS || "f6mbcvHCko",
+    database: process.env.DB_NAME || "XwcCLxaOuP",
+    port: process.env.DB_PORT || 3306
+});
+
+// const db = mysql.createConnection({
+//     server: "localhost",
+//     database: "android_final_project",
+//     user: "root",
+//     password: "123456"
+// })
 
 db.connect(err => {
     if (err) throw err;
